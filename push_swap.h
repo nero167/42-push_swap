80e7a2bde8fb3e3dc75c7e3f95a9928b36053623
nero167 <nerojan7@hotmail.com>
2024-06-19 15:39:55 +0200

    added header file

---
+++ push_swap.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: namirtha <namirtha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/22 13:51:50 by namirtha          #+#    #+#             */
+/*   Updated: 2024/06/19 15:01:19 by namirtha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdbool.h>
+# include <limits.h>
+# include "../libft/inc/libft.h"
+# include "../libft/inc/ft_printf.h"
+
+// creating structure --> It's a note!
+typedef struct s_stack_node
+{
+	int	nbr;
+	int	index;
+	int	push_cost;
+	bool	above_median;
+	bool	cheapest;
+	struct s_stack_node *target_node;
+	struct s_stack_node *next;
+	struct s_stack_node *prev;
+}				t_stack_node; // the whole struck defined as t_stack_node 
+
